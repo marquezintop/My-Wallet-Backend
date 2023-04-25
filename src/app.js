fb6298b23510ed43ad34c884cdd0336e971ae0f3
Bernardo Fonseca <marquez.cmbh@gmail.com>
2023-04-25 09:09:07 -0300

    Backend almost finalized

--- src/app.js
+++ src/app.js
@@ -29,8 +29,12 @@ const signUpSchema = joi.object({
 
 const signInSchema = joi.object({
     email: joi.string().email().required(),
-    password: joi.string().required().min(3)
+    password: joi.required()
   });
+const transactionsSchema = joi.object({
+    value: joi.number().positive().required(),
+    description: joi.string().required()
+})
 
 
 server.post("/sign-up", async (req,res) => {
@@ -47,9 +51,9 @@ server.post("/sign-up", async (req,res) => {
         if (checkingUser) return res.status(409).send("E-mail já cadastrado!");
         const encryptedPassword = bcrypt.hashSync(password, 10);
         await db.collection("users").insertOne({name, email, password: encryptedPassword});
-        res.status(201).send("Cadastrado com sucesso!!");
+        return res.status(201).send("Cadastrado com sucesso!!");
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
 })
 
@@ -66,14 +70,61 @@ server.post("/sign-in", async (req, res) => {
         const user = await db.collection("users").findOne({email});
         if (!user) return res.status(404).send("E-mail não está cadastrado");
         if (bcrypt.compareSync(password, user.password))  {
+            const username = user.name
             const token = uuid();
-            await db.collection("sessions").insertOne({userId: user._id,token})
-            return res.status(200).send(token)
+            const session = await db.collection("sessions").insertOne({userId: user._id,token, username})
+            return res.status(200).send(session)
         } else {
-            res.status(401).send("Senha errada!")
+           return res.status(401).send("Senha errada!")
         }
     } catch (err) {
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
+    }
+})
+
+server.post("/transactions", async (req,res) => {
+    const { value, description} = req.body;
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace('Bearer ', '');
+    if(!token) return res.sendStatus(401);
+
+    const validation = transactionsSchema.validate(req.body, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+
+    try {
+        const session = await db.collection("sessions").findOne({ token });
+        if (!session) return res.sendStatus(401);
+        const user = await db.collection("users").findOne({_id: session.userId})
+        if (user) {
+            await db.collection("transactions").insertOne({value, description, userId: session.userId});
+            return res.sendStatus(200);
+        } else {
+            return res.sendStatus(401);
+        }
+    } catch (err) {
+        return res.status(500).send(err.message);
+    }
+
+})
+
+server.get("/transactions", async (req,res) => {
+    const { authorization } = req.headers;
+
+    const token = authorization?.replace('Bearer ', '');
+    if(!token) return res.sendStatus(401);
+
+    try {
+        const session = await db.collection("sessions").findOne({ token });
+        if (!session) return res.sendStatus(401);
+        console.log(session.userId)
+        const transactions = await db.collection("transactions").find({userId: session.userId}).toArray()
+        res.status(201).send(transactions);
+    } catch (err) {
+        return res.status(500).send(err.message)
     }
 })
 
